f436a55391f6febfb7bbf8d3d8db6f0ac8551b60
shaheersaud2004 <shaheersaud2004s@gmail.com>
2024-01-06 23:39:18 -0500

    Added style

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.project-link').click(function() {
+        window.open($(this).attr('data-href'), '_blank');
+    });
+
+    $('header a[href*="#"]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        }, 500, 'linear');
+    });
+
+    $('.info-btn').click(function() {
+        var targetId = $(this).data('target');
+        $(targetId).toggle();
+    });
+});
