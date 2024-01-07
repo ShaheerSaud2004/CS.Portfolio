353b1802c6370f9073c738bac4fdc56404275fee
shaheersaud2004 <shaheersaud2004s@gmail.com>
2024-01-07 17:38:18 -0500

    Added more style

--- script.js
+++ script.js
@@ -34,6 +34,59 @@ function simpleType() {
             setTimeout(typeWriter, 50);
         }
     }
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Python', 'Java', 'HTML/CSS', 'C/C++', 'JavaScript', 'R'],
+            datasets: [{
+                label: 'Proficiency',
+                data: [98, 96, 94, 93, 92, 91, 90], // Example proficiency levels
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    var particles = [];
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    for (var i = 0; i < 100; i++) {
+        particles.push(new Particle());
+    }
+}
+
+function draw() {
+    background(255);
+    particles.forEach(function(p) {
+        p.update();
+        p.display();
+    });
+}
+
+function Particle() {
+    this.x = random(width);
+    this.y = random(height);
+    this.r = random(4, 8);
+    this.update = function() {
+        this.x += random(-2, 2);
+        this.y += random(-2, 2);
+    };
+    this.display = function() {
+        noStroke();
+        fill(0);
+        ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    };
+}
+
 
     typeWriter();
 }
